80e66098ac8ddb00332b107db6546331d349454e
AirtonHoffmann <airtonbocchese@gmail.com>
2020-06-23 17:08:14 -0300

    melhorias
    adicionado pontuacao e timer, e melhoris no codigo

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
+jogar = false
+
+erros = 0
+porcentagem = 100
+
 min = 1
 max = 20
 
@@ -8,6 +13,10 @@ figuara01.id = null
 figuara02 = new Object()
 figuara02.imagem = null
 figuara02.id = null
+
+ss = 0
+mm = 0
+hh = 0
 
 preloadImagem = new Array()
 for(i = 0; i < 21; i++){
@@ -39,54 +48,69 @@ function gerarAleatiro() {
     return num
 }
 
+function mudarImagem(idElemento, idImagem){
+    img = window.document.getElementById(idElemento)  
+    img.src = preloadImagem[idImagem].src
+}
+
 function revelar(id){
-    if(tabuleiro[id] != null){
+    if(tabuleiro[id] != null && jogar == true){
         if(figuara01.imagem == null || figuara02.imagem == null){
             if(figuara01.imagem == null){
                 figuara01.imagem = tabuleiro[id]
                 figuara01.id = id
-                mudarImagem(id)
+                mudarImagem(id, tabuleiro[id])
             }
             else if(id != figuara01.id){
                 figuara02.imagem = tabuleiro[id]
                 figuara02.id = id
-                mudarImagem(id)
+                mudarImagem(id, tabuleiro[id])
+                setTimeout(verificar, 500)
             }
         }
     }
-}
-
-function mudarImagem(id){
-    img = window.document.getElementById(id)  
-    img.src = preloadImagem[tabuleiro[id]].src
 }
 
 function verificar(){
     if(figuara01.imagem != null && figuara02.imagem != null){
         if(figuara01.imagem != figuara02.imagem){  
-            img = window.document.getElementById(figuara01.id)  
-            img.src = preloadImagem[0].src
-            img = window.document.getElementById(figuara02.id)  
-            img.src = preloadImagem[0].src
+            mudarImagem(figuara01.id, 0)
+            mudarImagem(figuara02.id, 0)
+            erros++
+            porcentagem = Math.trunc(20*100/(20+erros))
         }else{
             tabuleiro[figuara01.id] = null
             tabuleiro[figuara02.id] = null
         }
-        sleep(500)
         figuara01.imagem = null
         figuara01.id = null
         figuara02.imagem = null
         figuara02.id = null
     }
-    
 }
 
-setInterval(verificar, 100)
+function comecarJogo(){
+    if(jogar == false){
+        jogar = true
+        setInterval(() => { timer(); }, 1000)
+        img = window.document.getElementById('botao')  
+        img.src = 'imagens/jogar.png'
+    }else{
+        document.location.reload()
+    }  
+}
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-        currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
+function timer() {
+    ss++; 
+    if (ss == 59) { 
+        ss = 0 
+        mm++ 
+        if (mm == 59) { 
+            mm = 0
+            hh++
+        }
+    }
+
+    format = '<strong>Points '+porcentagem+'% &nbsp;&nbsp;&nbsp; Time '+(hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss)+'</strong>'
+    document.getElementById('pontos').innerHTML = format
 }
